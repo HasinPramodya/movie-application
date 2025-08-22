08bdd1054269ee46de1270a1d395acf366640292
hasin.pramodya <pramodyahasin@gmail.com>
2025-08-22 18:57:25 +0530

    add Theme provider for the RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from 'next-themes'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/component/Header";
+import ThemeChanger from "@/app/component/ThemeCom";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider>
         <Header/>
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
